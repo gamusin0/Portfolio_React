3d897fc32a6fcf6422eb22e51c734a31d18e5364
Alejandra Huguet Aznar <ultimogamusino@gmail.com>
2025-12-09 00:16:57 +0100

    Cambio de nombres y ajuste del overlay sobre la pantalla

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,11 +3,11 @@ import { Menu, X } from "lucide-react";
 import { useEffect, useState } from "react";
 
 const navItems = [
-    {name: "Home", href: "#hero"},
-    {name: "About", href: "#aboutme"},
-    {name: "Skills", href: "#skills"},
-    {name: "Projects", href: "#projects"},
-    {name: "Contact", href: "#contact"},
+    {name: "Inicio", href: "#hero"},
+    {name: "Sobre mí", href: "#aboutme"},
+    {name: "Habilidades", href: "#skills"},
+    {name: "Proyectos", href: "#projects"},
+    {name: "Contacto", href: "#contact"},
 ];
 
 export const Navbar =() => {
@@ -56,27 +56,27 @@ export const Navbar =() => {
         </button>
 
         <div
-          className={cn(
-            "fixed inset-0 bg-background/95 backdroup-blur-md z-40 flex flex-col items-center justify-center",
-            "transition-all duration-300 md:hidden",
-            isMenuOpen
-              ? "opacity-100 pointer-events-auto"
-              : "opacity-0 pointer-events-none"
-          )}
-        >
-          <div className="flex flex-col space-y-8 text-xl">
-            {navItems.map((item, key) => (
-              <a
-                key={key}
-                href={item.href}
-                className="text-foreground/80 hover:text-primary transition-colors duration-300"
-                onClick={() => setIsMenuOpen(false)}
-              >
-                {item.name}
-              </a>
-            ))}
-          </div>
-        </div>
+            className={cn(
+              "fixed inset-0 h-screen bg-background/95 backdrop-blur-md z-50 flex flex-col items-center justify-center",
+              "transition-all duration-300 md:hidden",
+              isMenuOpen
+                ? "opacity-100 pointer-events-auto"
+                : "opacity-0 pointer-events-none"
+            )}
+          >
+            <div className="flex flex-col space-y-8 text-2xl">
+              {navItems.map((item, key) => (
+                <a
+                  key={key}
+                  href={item.href}
+                  className="text-foreground/80 hover:text-primary transition-colors duration-300"
+                  onClick={() => setIsMenuOpen(false)}
+                >
+                  {item.name}
+                </a>
+              ))}
+            </div>
+          </div>       
 
 
     </div>
